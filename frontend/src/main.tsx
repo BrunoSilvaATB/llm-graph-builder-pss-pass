2bfb94f0d318c4d3d7717e365afcf04c4401e3fc
Bruno Silva <silva@atb-bremen.de>
2026-01-28 11:32:52 +0100

    Added debug tests

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,11 @@ import Auth0ProviderWithHistory from './components/Auth/Auth.tsx';
 import App from './App.tsx';
 import { SKIP_AUTH } from './utils/Constants.ts';
 
+console.log('VITE_SKIP_AUTH:', import.meta.env.VITE_SKIP_AUTH);
+console.log('VITE_REACT_APP_SOURCES:', import.meta.env.VITE_REACT_APP_SOURCES);
+console.log('All env vars:', import.meta.env);
+console.log('VITE_TEST_VAR:', import.meta.env.VITE_TEST_VAR);
+console.log('This is a frontend test created by silva@atb-bremen.de');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     {SKIP_AUTH ? (
